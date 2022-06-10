128831ec09a66abd4fc2686077cbb4df9a2c88dc
mothnox <52550171+mothnox@users.noreply.github.com>
2022-06-10 17:20:01 -0500

    Update lua-bytecode.lua

--- lua-bytecode.lua
+++ lua-bytecode.lua
@@ -1339,7 +1339,7 @@ local function parse_or_convert_bytecode(bytecode_as_string_or_loader, convert_t
             location_in_file = location_in_file..(" %02X"):format(data_in_file:byte(j))
          end
          local target_length = #"+FFFF:Size=A: 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E 1F 20 21 22 23 24 25 26 27 28 29 2A 2B 2C 2D 2E 2F 30 31 32 33 34 35 36 37 38 39 3A 3B 3C 3D 3E 3F 40 41 42 43 44 45 46 47 48 49 4A 4B 4C 4D 4E 4F"
-         -- if #data_in_file > 30 or #location_in_file > target_length then
+         -- if #data_in_file > 64 or #location_in_file > target_length then
             -- location_in_file = location_in_file:sub(1, target_length - 3).."..."
          --end
          location_in_file = location_in_file..(" "):rep(target_length - #location_in_file)
